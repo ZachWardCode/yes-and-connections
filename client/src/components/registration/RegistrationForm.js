381c3bb030ca25f9f08c845969bb8efb0bad7b94
ZachWardCode <zachary.ward0094@gmail.com>
2023-02-14 09:05:49 -0500

    updated user model and user authentication to include firstName, lastName, bio. Added cleanUserInput for later

--- client/src/components/registration/RegistrationForm.js
+++ client/src/components/registration/RegistrationForm.js
@@ -7,6 +7,9 @@ const RegistrationForm = () => {
     email: "",
     password: "",
     passwordConfirmation: "",
+    firstName: "",
+    lastName: "",
+    bio: ""
   });
 
   const [errors, setErrors] = useState({});
@@ -15,7 +18,7 @@ const RegistrationForm = () => {
 
   const validateInput = (payload) => {
     setErrors({});
-    const { email, password, passwordConfirmation } = payload;
+    const { email, password, passwordConfirmation, firstName, lastName } = payload;
     const emailRegexp = config.validation.email.regexp;
     let newErrors = {};
     if (!email.match(emailRegexp)) {
@@ -46,6 +49,20 @@ const RegistrationForm = () => {
       }
     }
 
+    if (firstName.trim() == "") {
+      newErrors = {
+        ...newErrors,
+        firstName: "is required",
+      };
+    }
+
+    if (lastName.trim() == "") {
+      newErrors = {
+        ...newErrors,
+        lastName: "is required",
+      }
+    }
+  
     setErrors(newErrors);
   };
 
@@ -89,11 +106,46 @@ const RegistrationForm = () => {
     <div className="grid-container">
       <h1>Register</h1>
       <form onSubmit={onSubmit}>
+      <div>
+          <label>
+            First Name
+            <input
+              type="text"
+              name="firstName"
+              value={userPayload.firstName}
+              onChange={onInputChange}
+            />
+            <FormError error={errors.firstName} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name
+            <input
+              type="text"
+              name="lastName"
+              value={userPayload.lastName}
+              onChange={onInputChange}
+            />
+            <FormError error={errors.lastName} />
+          </label>
+        </div>
         <div>
           <label>
             Email
             <input type="text" name="email" value={userPayload.email} onChange={onInputChange} />
             <FormError error={errors.email} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Bio (Not Required)
+            <input
+              type="text"
+              name="bio"
+              value={userPayload.bio}
+              onChange={onInputChange}
+            />
           </label>
         </div>
         <div>
