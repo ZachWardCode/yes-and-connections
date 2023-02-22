a1b5e1f90111b720067e6027f8b67473ada06648
ZachWardCode <zachary.ward0094@gmail.com>
2023-02-22 17:09:03 -0500

    updated theaters page info and seeders

--- client/src/components/TheatersListPage.js
+++ client/src/components/TheatersListPage.js
@@ -39,7 +39,14 @@ const TheatersListPage = props => {
 
   return (
     <div>
-      <h2>Improv is fun! Find a theater! Find a show!</h2>
+      <h1>Yes And Connections</h1>
+      <h3>The Best Platform for Improv Performers to Stay Connected</h3>
+      <h4>What is Yes and Connections?</h4>
+      <p>Yes And Connections is a platform made for improv performers, by an improv performer. Performing at an improv theaters is a wonderful experience and the community and friends it brings you is often the best part! This site is the best place to stay connected with your fellow cast members at the theater you perform at!</p> 
+      <p>Bonding and connection plays a huge role in improv and Yes And Connections makes sure your cast stays in touch with each other as well as up-to-date with what's happening at your theater. This leads a stronger community, happier performers, and, wouldn't you know it, better shows!</p> 
+      <p>If you are the owner of a theater that isn't using this site, register now! Your performers will thank you and your community will become even closer. We all know that performers come for the laugh, but in the end we stay for the people.</p>
+      <p>Yes And Connections breaks down the barriers of mismatching social media use and get rid of the difficulty of performers needing to maintain handfuls of group chats just to stay in the loop and makes plans with their friends for showtime.</p> 
+      <p>If you're a performer at a theater that is registered on this site, find your theater below and sign in or sign up if you haven't and get connected to your community! If you're a performer at a theater that hasn't registered, reach out to management and let them know you've found Yes And Connections and the theater needs to register to make your life and theirs a whole lot easier!</p>
       {theaterTiles}
     </div>
   )
