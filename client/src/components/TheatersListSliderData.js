8cf96c60e222f4e90769cd6c8aaefb6cff0f97e2
ZachWardCode <zachary.ward0094@gmail.com>
2023-02-24 00:28:28 -0500

    Add styling and image slider to theaters list page

---
+++ client/src/components/TheatersListSliderData.js
@@ -0,0 +1,27 @@
+import theatersListImage1 from "../assets/images/theatersListPageImages/theatersListImage1.jpeg"
+import theatersListImage2 from "../assets/images/theatersListPageImages/theatersListImage2.jpeg"
+import theatersListImage3 from "../assets/images/theatersListPageImages/theatersListImage3.jpeg"
+import theatersListImage4 from "../assets/images/theatersListPageImages/theatersListImage4.jpeg"
+import theatersListImage5 from "../assets/images/theatersListPageImages/theatersListImage5.jpeg"
+import theatersListImage6 from "../assets/images/theatersListPageImages/theatersListImage6.jpeg"
+
+export const TheatersListSliderData = [
+  {
+    image: theatersListImage1
+  },
+  {
+    image: theatersListImage2
+  },
+  {
+    image: theatersListImage3
+  },
+  {
+    image: theatersListImage4
+  },
+  {
+    image: theatersListImage5
+  },
+  {
+    image: theatersListImage6
+  }
+]
